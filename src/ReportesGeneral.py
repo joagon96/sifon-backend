448cc84063045f472edd2d003dcb2f98c6992b5c
joagon96 <joagon96@gmail.com>
2023-04-16 12:22:53 -0300

    remove sqlite dependency from reportes

--- src/ReportesGeneral.py
+++ src/ReportesGeneral.py
@@ -1,5 +1,4 @@
-import sqlite3
-from src.executeQuerys import getQueryScalar
+from src.executeQuerys import getQueryScalar, getQuerySum
 
 def ContadorClientes():
     return getQueryScalar("SELECT COUNT(*) FROM Cliente")
@@ -14,22 +13,10 @@ def ContadorRepartos():
     return getQueryScalar("SELECT COUNT(*) FROM Reparto")
 
 def ContadorBidones12():   
-    return calcularSumatoria("SELECT com12 FROM Reparto")
+    return getQuerySum("SELECT com12 FROM Reparto")
 
 def ContadorBidones20():
-    return calcularSumatoria("SELECT com20 FROM Reparto")
+    return getQuerySum("SELECT com20 FROM Reparto")
 
 def ContadorSoda():
-    return calcularSumatoria("SELECT comSoda FROM Reparto")
-
-def calcularSumatoria(query):
-     with sqlite3.connect("sifono_db.db") as con:
-            c = con.cursor()        
-            c.execute(query)
-            cantidad=c.fetchall()
-            suma=0
-            for i in cantidad:
-                for j in i:
-                    suma = suma + j 
-            return suma
-            con.close()
+    return getQuerySum("SELECT comSoda FROM Reparto")
