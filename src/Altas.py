6b09f8b52702041541890e3b6710313c52a421b9
Tomim19 <tomim19@outlook.com>
2023-06-11 21:56:07 -0300

    cleanup

--- src/Altas.py
+++ src/Altas.py
@@ -3,30 +3,38 @@ from src.executeQuerys import executeQuery
 from datetime import datetime
 from werkzeug.security import generate_password_hash
 
+
 def altaCliente():
-    name = request.form.get('nombreApellido')       
+    name = request.form.get('nombreApellido')
     dom = request.form.get('domicilio')
     tel = request.form.get('telefono')
     idz = request.form.get('idZona')
     hab = 1
-    executeQuery("INSERT INTO Cliente(idZona,nomapeCli,domicilio,telefonoCli,habilitadoC) VALUES (?,?,?,?,?)",(idz, name.capitalize(),dom.capitalize(),tel,hab,))
+    executeQuery("INSERT INTO Cliente(idZona,nomapeCli,domicilio,telefonoCli,habilitadoC) VALUES (?,?,?,?,?)",
+                 (idz, name.capitalize(), dom.capitalize(), tel, hab,))
+
 
 def altaRepartidor():
-    name = request.form.get('nombreApellido')       
+    name = request.form.get('nombreApellido')
     tel = request.form.get('telefono')
-    hab = 1     
-    executeQuery("INSERT INTO Repartidor(nomapeRep,telefonoRep,habilitadoRep) VALUES (?,?,?)",(name.capitalize(),tel,hab,))
-    executeQuery("INSERT INTO Usuario(usuario,password_hash, role) VALUES(?,?,?)",(name.capitalize(), generate_password_hash("repartidor", method='sha256'),"repartidor",))
+    hab = 1
+    executeQuery("INSERT INTO Repartidor(nomapeRep,telefonoRep,habilitadoRep) VALUES (?,?,?)",
+                 (name.capitalize(), tel, hab,))
+    executeQuery("INSERT INTO Usuario(usuario,password_hash, role) VALUES(?,?,?)", (name.capitalize(
+    ), generate_password_hash("repartidor", method='sha256'), "repartidor",))
 
-def altaReparto():     
+
+def altaReparto():
     idr = request.form.get('idRepartidor')
     dia = request.form.get('dia')
     idz = request.form.get('idZona')
-    hab = 1  
-    executeQuery("INSERT INTO Reparto(idRepartidor,dia,habilitadoReparto,idZona) VALUES (?,?,?,?)",(idr,dia,hab,idz))
+    hab = 1
+    executeQuery(
+        "INSERT INTO Reparto(idRepartidor,dia,habilitadoReparto,idZona) VALUES (?,?,?,?)", (idr, dia, hab, idz))
+
 
 def altaLineaReparto():
-    idc = request.form.get('idCliente')       
+    idc = request.form.get('idCliente')
     idr = request.form.get('idReparto')
     est12 = request.form.get('est12')
     est20 = request.form.get('est20')
@@ -39,24 +47,30 @@ def altaLineaReparto():
     de12 = 0
     de20 = 0
     deS = 0
-    executeQuery("INSERT INTO LineaReparto(idCliente,idReparto,est12,est20,estSoda,com12,com20,comSoda,pago,fiado,dev12,dev20,devSoda) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?)",(idc,idr,est12,est20,estSoda,co12,co20,coms,pag,fia,de12,de20,deS,))
+    executeQuery("INSERT INTO LineaReparto(idCliente,idReparto,est12,est20,estSoda,com12,com20,comSoda,pago,fiado,dev12,dev20,devSoda) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?)",
+                 (idc, idr, est12, est20, estSoda, co12, co20, coms, pag, fia, de12, de20, deS,))
+
 
 def altaZona():
     desc = request.form.get('descripcionZona')
-    hab = 1              
-    executeQuery("INSERT INTO Zona(descripcion, habilitado) VALUES (?,?)",(desc.capitalize(),hab,))
+    hab = 1
+    executeQuery("INSERT INTO Zona(descripcion, habilitado) VALUES (?,?)",
+                 (desc.capitalize(), hab,))
+
 
 def altaHistorico():
-    idr = request.form.get('repartidor')   
-    fech = request.form.get('fecha') 
+    idr = request.form.get('repartidor')
+    fech = request.form.get('fecha')
     zona = request.form.get('zona')
-    dia = request.form.get('dia')      
-    executeQuery("INSERT INTO Historico(repartidor,fecha,zona,dia) VALUES (?,?,?,?)",(idr,fech,zona,dia,))
+    dia = request.form.get('dia')
+    executeQuery(
+        "INSERT INTO Historico(repartidor,fecha,zona,dia) VALUES (?,?,?,?)", (idr, fech, zona, dia,))
+
 
 def altaHistoricoLinea():
     idh = request.form.get('idHistorico')
-    cli = request.form.get('cliente')   
-    domicilio = request.form.get('domicilio') 
+    cli = request.form.get('cliente')
+    domicilio = request.form.get('domicilio')
     com12 = request.form.get('com12')
     com20 = request.form.get('com20')
     comSoda = request.form.get('comSoda')
@@ -65,20 +79,27 @@ def altaHistoricoLinea():
     dev12 = request.form.get('dev12')
     dev20 = request.form.get('dev20')
     devSoda = request.form.get('devSoda')
-    observacion = request.form.get('observacion')      
-    executeQuery("INSERT INTO HistoricoLinea(idHistorico,cliente,domicilio,com12,com20,comSoda,pago,fiado,dev12,dev20,devSoda,observacion) VALUES (?,?,?,?,?,?,?,]?,?,?,?,?)",(idh,cli,domicilio,com12,com20,comSoda,pago,fiado,dev12,dev20,devSoda,observacion,))
+    observacion = request.form.get('observacion')
+    executeQuery("INSERT INTO HistoricoLinea(idHistorico,cliente,domicilio,com12,com20,comSoda,pago,fiado,dev12,dev20,devSoda,observacion) VALUES (?,?,?,?,?,?,?,]?,?,?,?,?)",
+                 (idh, cli, domicilio, com12, com20, comSoda, pago, fiado, dev12, dev20, devSoda, observacion,))
+
 
 def habCliente():
-    idCliente2 = request.form.get('idCliente')       
-    hab = 1      
-    executeQuery("UPDATE Cliente SET habilitadoC=? WHERE idCliente = ?", (hab,idCliente2,) )
+    idCliente2 = request.form.get('idCliente')
+    hab = 1
+    executeQuery(
+        "UPDATE Cliente SET habilitadoC=? WHERE idCliente = ?", (hab, idCliente2,))
+
 
 def habReparto():
-    idReparto2 = request.form.get('idReparto')             
-    hab = 1      
-    executeQuery("UPDATE Reparto SET habilitadoReparto=? WHERE idReparto = ?", (hab,idReparto2,) )
+    idReparto2 = request.form.get('idReparto')
+    hab = 1
+    executeQuery(
+        "UPDATE Reparto SET habilitadoReparto=? WHERE idReparto = ?", (hab, idReparto2,))
+
 
 def habRepartidor():
-    idRepartidor2 = request.form.get('idRepartidor')             
-    hab = 1     
-    executeQuery("UPDATE Repartidor SET habilitadoRep=? WHERE idRepartidor = ?", (hab,idRepartidor2,))
+    idRepartidor2 = request.form.get('idRepartidor')
+    hab = 1
+    executeQuery(
+        "UPDATE Repartidor SET habilitadoRep=? WHERE idRepartidor = ?", (hab, idRepartidor2,))
--- src/Auth.py
+++ src/Auth.py
@@ -1,28 +1,33 @@
 from flask import request, jsonify, current_app
-import jwt 
+import jwt
 from functools import wraps
 from werkzeug.security import generate_password_hash, check_password_hash
 from src.executeQuerys import executeQuery, getQueryData
+
 
 def Register():
     usuario = request.form.get('username')
     input_pwd = request.form.get('password')
     pwd = generate_password_hash(input_pwd, method='sha256')
-    executeQuery("INSERT INTO Usuario(usuario,password_hash, role) VALUES(?,?,?)",(usuario, pwd, "admin",))
+    executeQuery("INSERT INTO Usuario(usuario,password_hash, role) VALUES(?,?,?)",
+                 (usuario, pwd, "admin",))
     return jsonify("OK")
+
 
 def Login():
     input_pwd = request.form.get('password')
     username = request.form.get('username')
-    data = getQueryData("SELECT id,usuario,password_hash,role FROM Usuario WHERE usuario = ?", (username,))
-    if len(data)>0 and check_password_hash(data[0]['password_hash'], input_pwd):
+    data = getQueryData(
+        "SELECT id,usuario,password_hash,role FROM Usuario WHERE usuario = ?", (username,))
+    if len(data) > 0 and check_password_hash(data[0]['password_hash'], input_pwd):
         token = jwt.encode({
             'user': username,
             'role': data[0]['role']
-        },current_app.config['SECRET_KEY'])
+        }, current_app.config['SECRET_KEY'])
         return jsonify({"token": token, "username": username, "role": data[0]['role']})
     else:
         return jsonify({"error": "Invalid credentials"}), 401
+
 
 def token_required(f):
     @wraps(f)
@@ -43,20 +48,24 @@ def token_required(f):
 
         try:
             token = auth_headers[1]
-            data = jwt.decode(token, current_app.config['SECRET_KEY'], algorithms=['HS256'])
-            user_data = getQueryData("SELECT id,usuario,password_hash,role FROM Usuario WHERE usuario = ?", (data['user'],))
+            data = jwt.decode(
+                token, current_app.config['SECRET_KEY'], algorithms=['HS256'])
+            user_data = getQueryData(
+                "SELECT id,usuario,password_hash,role FROM Usuario WHERE usuario = ?", (data['user'],))
             if len(user_data) == 0:
                 raise RuntimeError('User not found')
             return f(*args, **kwargs)
         except jwt.ExpiredSignatureError:
-            return jsonify(expired_msg), 401 
+            return jsonify(expired_msg), 401
         except (jwt.InvalidTokenError, Exception) as e:
             return jsonify(invalid_msg), 401
 
-    return _verify    
+    return _verify
+
 
 def CurrentUser():
     auth_headers = request.headers.get('Authorization', '').split()
     token = auth_headers[1]
-    data = jwt.decode(token, current_app.config['SECRET_KEY'], algorithms=['HS256'])
+    data = jwt.decode(
+        token, current_app.config['SECRET_KEY'], algorithms=['HS256'])
     return jsonify({"username": data['user'], "role": data['role']})
